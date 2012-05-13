a7c81484181410531c98e411be9e246c71b1c628
jboortz <juliaann11@gmail.com>
2012-05-13 00:41:57 -0400

    Updated populate shifts

--- Scheduler/populateShifts.py
+++ Scheduler/populateShifts.py
@@ -15,32 +15,33 @@ def add_shifts():
         if d != datetime.date(current_year, 12, 25):
             s = Shift(date = d, time = 0, hasAdultSki = True, hasAdultBoard = True)
             if d.weekday() == 5:
-                hasChildrenSki = True
-                hasChildrensBoard = True
-                hasRace = True
+                s.hasChildrensSki = True
+                s.hasChildrensBoard = True
+                s.hasRace = True
             elif d.weekday() == 6:
-                hasChildrenSki = True
-                hasChildrensBoard = True
-                hasRace = False
+                s.hasChildrensSki = True
+                s.hasChildrensBoard = True
+                s.hasRace = False
             elif d > datetime.date(current_year, 12, 22) and d < datetime.date(current_year + 1, 1, 4):
-                hasChildrenSki = True
-                hasChildrensBoard = True
-                hasRace = False
+                s.hasChildrensSki = True
+                s.hasChildrensBoard = True
+                s.hasRace = False
             else:
-                hasChildrenSki = False
-                hasChildrensBoard = False
-                hasRace = False
+                s.hasChildrensSki = False
+                s.hasChildrensBoard = False
+                s.hasRace = False
+            print s
             s.save()
         if d != datetime.date(current_year, 12, 24):
             s = Shift(date = d, time = 1, hasAdultSki = True, hasAdultBoard = True,
-                      hasChildrensSki = True, hasChildrensBoard = True, hasRace = True)
+                      hasChildrensSki = False, hasChildrensBoard = False, hasRace = False)
             s.save()
         if d.weekday() == 4 or d.weekday() == 5:
             s = Shift(date = d, time = 2, hasAdultSki = True, hasAdultBoard = True,
-                      hasChildrensSki = True, hasChildrensBoard = True, hasRace = True)
+                      hasChildrensSki = False, hasChildrensBoard = False, hasRace = False)
             s.save()
         elif d > datetime.date(current_year, 12, 22) and d < datetime.date(current_year + 1, 1, 4):
             s = Shift(date = d, time = 2, hasAdultSki = True, hasAdultBoard = True,
-                      hasChildrensSki = True, hasChildrensBoard = True, hasRace = True)
+                      hasChildrensSki = False, hasChildrensBoard = False, hasRace = True)
             s.save()
             
